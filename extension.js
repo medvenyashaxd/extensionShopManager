6bd75acdc60eb2c0bfd8fbeb3206778b0456b001
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-11-12 21:38:54 +0300

    added main file

---
+++ extension.js
@@ -0,0 +1,30 @@
+let inputProductNameText = document.querySelector('input.productModalQuery').value.split(' ')
+let productsListHtml = document.querySelectorAll('td.issue-info')
+
+
+function parsingAllTdInfo () {
+    for (let htmlData of productsListHtml) {
+        styledTdInfo(htmlData)
+    }
+}
+
+function styledTdInfo (tdHtmlElement) {
+    let count = 0
+    let productNameHtml = tdHtmlElement.querySelector('small')
+    let productNameText = productNameHtml.textContent.split(' ')
+    let commonWords = ""
+
+    for (let word of productNameText) {
+        if (inputProductNameText.includes(word)) {
+           commonWords += `<span style="color: red; font-weight: bold;">${word}</span> `
+        }
+        else {
+            commonWords += `${word} `
+        }
+    }
+
+    productNameHtml.getElementsByTagName("a")[count].innerHTML = commonWords
+    count++
+}
+
+parsingAllTdInfo()
