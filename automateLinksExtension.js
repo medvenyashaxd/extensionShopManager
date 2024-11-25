d36f6d9dc6830241cdbf1b94d93d233655751bbd
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-11-25 17:34:21 +0300

    init structure

--- automateLinksExtension.js
+++ automateLinksExtension.js
@@ -1,29 +1,48 @@
-// let table = document.querySelector("tbody")
-//
-// let checkRows = (records) => {
-//     for (let record of records) {
-//         if (record.addedNodes.length) {
-//             let element = document.getElementById(record.addedNodes[0].data)
-//             if (element) {
-//                 console.log(element)
-//             }
-//         }
-//     }
-// }
-//
-// let observer = new MutationObserver(checkRows)
-// observer.observe(
-//     table,
-//     {
-//         childList: true,
-//         subtree: true
-//     }
-// )
+let table = document.querySelector("tbody")
 
+let checkRows = (records) => {
+    for (let record of records) {
+        if (record.addedNodes.length) {
+            let element = document.getElementById(record.addedNodes[0].data)
+            if (element) {
+                console.log(element)
+            }
+        }
+    }
+}
 
-let id = document.getElementById("id")
-let brand = id.querySelector(".brandAndName")
-let smallBrandContent = brand.querySelector("small").outerText
-brand.innerHTML = smallBrandContent + "<br><span></span>"
-let linked_name = id.querySelector(".btn.btn-xs.btn-outline.btn-primary")
-linked_name.innerHTML = '<i class="fa fa-external-link"></i><span style="background-color: yellow;"> new text</span>'
+let observer = new MutationObserver(checkRows)
+observer.observe(
+    table,
+    {
+        childList: true,
+        subtree: true
+    }
+)
+
+function getText(element) {
+    let brand = element.querySelector(".brandAndName")
+    let smallBrandContent = brand.querySelector("small").outerText
+    let brandText = brand.innerText
+    brand.innerHTML = smallBrandContent + "<br><span></span>"
+
+    let linked_name = element.querySelector(".btn.btn-xs.btn-outline.btn-primary").text
+    linked_name.innerHTML = '<i class="fa fa-external-link"></i><span style="background-color: yellow;"> new text</span>'
+}
+
+let brandAndName = "ИНТЕРСКОЛ 862.2.2.70 Шуруповерт Интерскол ШАУ-250/36ВЭ".split(" ")
+let testName = "ИНТЕРСКОЛ ШАУ-250/36ВЭ 862.2.2.70".split(" ")
+
+function parsingWords(words1, words2) {
+    let commonWords = ""
+    for (let word of words1) {
+        if (words2.includes(word)) {
+            commonWords += `<span style="background-color: yellow; color: #222; font-weight: bold;">${word}</span> `
+        } else {
+            commonWords += `<span>${word}</span> `
+        }
+    }
+    return commonWords
+}
+
+console.log(parsingWords(brandAndName, testName))
