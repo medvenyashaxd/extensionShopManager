6b1926d988f99656c448631ba4de0cc4023ff955
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-11-28 22:57:09 +0300

    testing code in shopManager

--- automateLinksExtension.js
+++ automateLinksExtension.js
@@ -1,52 +1,41 @@
-// let table = document.querySelector("tbody")
-//
-// let checkRows = (records) => {
-//     for (let record of records) {
-//         if (record.addedNodes.length) {
-//             let element = document.getElementById(record.addedNodes[0].data)
-//             if (element) {
-//                 parsingWords(element)
-//             }
-//         }
-//     }
-// }
-//
-// let observer = new MutationObserver(checkRows)
-// observer.observe(
-//     table,
-//     {
-//         childList: true,
-//         subtree: true
-//     }
-// )
-//
-function getText() {
-    // let brand = element.querySelector(".brandAndName")
-    // let brandText = brand.innerText.split("\n")[1]
-    // // let smallBrandContent = brand.querySelector("small").outerText
-    // // brand.innerHTML = smallBrandContent + "<br><span></span>"
-    //
-    //
-    // let linked_name = element.querySelector(".btn.btn-xs.btn-outline.btn-primary").text
-    // // linked_name.innerHTML = '<i class="fa fa-external-link"></i><span style="background-color: yellow;"> new text</span>'
-    // return {
-    //     brandText: brandText,
-    //     linked_name: linked_name,
-    let brandAndName = "ИНТЕРСКОЛ 862.2.2.70 Шуруповерт Интерскол ШАУ-250/36ВЭ".split(" ")
-    let testName = "ИНТЕРСКОЛ ШАУ-250/36ВЭ 862.2.2.70".split(" ")
+let table = document.querySelector("tbody")
+
+let checkRows = (records) => {
+    for (let record of records) {
+        if (record.addedNodes.length) {
+            let element = document.getElementById(record.addedNodes[0].data)
+            if (element) {
+                parsingWords(element)
+            }
+        }
+    }
+}
+
+let observer = new MutationObserver(checkRows)
+observer.observe(
+    table,
+    {
+        childList: true,
+        subtree: true
+    }
+)
+
+
+function getText(elem) {
+    let brandText = elem.querySelector(".brandAndName").innerText.split("\n")[1].split(" ")
+    let linkedText = elem.querySelector(".btn.btn-xs.btn-outline.btn-primary").text.split(" ")
     return {
-        brandText: brandAndName,
-        linked_name: testName,
+        brandText: brandText,
+        linkedText: linkedText,
     }
 }
 
 
-function parsingWords() {
-    let words = getText()
-    let res1 = getCommonWords(words.brandText, words.linked_name)
-    let res2 = getCommonWords(words.linked_name, words.brandText)
-    console.log(res1)
-    console.log(res2)
+function parsingWords(elem) {
+    let words = getText(elem)
+    let brandText = getCommonWords(words.brandText, words.linkedText)
+    let linkedText = getCommonWords(words.linkedText, words.brandText)
+    insertTextInElement(elem, brandText, linkedText)
 }
 
 
@@ -54,15 +43,15 @@ function getCommonWords(words1, words2) {
     let commonWords = ""
     for (let word of words1) {
         if (words2.includes(word)) {
-            commonWords += styledWords(true, word)
+            commonWords += insertStyle(true, word)
         } else {
-            commonWords += styledWords(false, word)
+            commonWords += insertStyle(false, word)
         }
     }
     return commonWords
 }
 
-function styledWords(common, word) {
+function insertStyle(common, word) {
     if (common) {
         return `<span style="background-color: yellow; color: #222; font-weight: bold;">${word}</span> `
     } else {
@@ -70,4 +59,11 @@ function styledWords(common, word) {
     }
 }
 
-parsingWords()
+function insertTextInElement(elem, brandText, linkedText) {
+    let brandElem = elem.querySelector(".brandAndName")
+    let smallBrandContent = brandElem.querySelector("small").outerText
+    brandElem.innerHTML = smallBrandContent + "<br>" + brandText
+
+    let linkedElem = elem.querySelector(".btn.btn-xs.btn-outline.btn-primary")
+    linkedElem.innerHTML = '<i class="fa fa-external-link"></i>' + linkedText
+}
