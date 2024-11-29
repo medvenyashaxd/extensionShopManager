d6d0914c78761109723858a5e46c687ae5e5d231
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-11-29 10:56:17 +0300

    return color #222 in style

--- automateLinksExtension.js
+++ automateLinksExtension.js
@@ -53,7 +53,7 @@ function getCommonWords(words1, words2) {
 
 function insertStyle(common, word) {
     if (common) {
-        return `<span style="background-color: yellow; color: #222</span> `
+        return `<span style="background-color: yellow; color: #222">${word}</span> `
     } else {
         return `<span>${word}</span> `
     }
