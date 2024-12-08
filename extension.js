5818f0276e193da76dd94f7436613193c95614d4
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-12-08 17:07:38 +0300

    optimizing code for automate links code

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 let productModalResults = document.querySelector(".productModalResults")
 
-let observer = new MutationObserver(getMutationRecord)
+let observer = new MutationObserver(checkModalResults)
 
-function getMutationRecord(records) {
+function checkModalResults(records) {
     for (let record of records) {
         let nodes = record.addedNodes
         if (nodes.length) {
@@ -11,38 +11,47 @@ function getMutationRecord(records) {
     }
 }
 
+function removeBlackCharacters(str) {
+    return str.replace(/"/g, "").replace(/,/g, "").replace(/;/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/\(/g, "").replace(/\)/g, "").trim()
+}
+
 function parsingAllTdInfo(productsListHtml) {
-    for (let htmlData of productsListHtml) {
-        styledTdInfo(htmlData)
+    let inputProductNameText = removeBlackCharacters(document.querySelector('input.productModalQuery').value)
+    for (let htmlElem of productsListHtml) {
+        let smallElem = htmlElem.querySelector('small')
+        let productNameText = removeBlackCharacters(smallElem.textContent)
+        insertCommonWordsInTdInfo(smallElem, getCommonWords(productNameText.split(" "), inputProductNameText, true))
     }
 }
 
-function searchIncludesText(words, word){
+function searchIncludesText(words, word) {
     words = words.toLowerCase().split(" ")
     word = word.toLowerCase()
     return words.includes(word)
 }
 
-function styledTdInfo(tdHtmlElement) {
-    let inputProductNameText = document.querySelector('input.productModalQuery').value.replace(/"/g, "").replace(/,/g, "").replace(/;/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/\(/g, "").replace(/\)/g, "").trim()
-    console.log(inputProductNameText)
+function insertCommonWordsInTdInfo(htmlElem, commonWords) {
+    htmlElem.getElementsByTagName("a")[0].innerHTML = commonWords
+}
 
-    let productNameHtml = tdHtmlElement.querySelector('small')
-
-    let productNameText = productNameHtml.textContent.replace(/"/g, "").replace(/,/g, "").replace(/;/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/\(/g, "").replace(/\)/g, "").trim().split(' ')
-    console.log(productNameText)
-
+function getCommonWords(words1, words2, td) {
     let commonWords = ""
-    for (let word of productNameText) {
-        if (searchIncludesText(inputProductNameText, word)) {
-            commonWords += `<span style="background-color: yellow; color: #222; font-weight: bold;">${word}</span> `
+    for (let word of words1) {
+        if (searchIncludesText(words2, word)) {
+            commonWords += insertStyle(true, word, td)
         } else {
-            commonWords += `<span>${word}</span> `
+            commonWords += insertStyle(false, word, td)
         }
     }
-    productNameHtml.getElementsByTagName("a")[0].innerHTML = commonWords
+    return commonWords
+}
 
-    console.log(commonWords)
+function insertStyle(common, word, td) {
+    if (common) {
+        return `<span style="background-color: yellow; color: #222;${td ? 'font-weight: bold;' : ''}">${word}</span> `;
+    } else {
+        return `<span>${word}</span> `;
+    }
 }
 
 observer.observe(
