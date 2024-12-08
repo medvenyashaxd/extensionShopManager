8d44761fedab5701d6df67c1a95894c0367a791a
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-12-08 17:28:34 +0300

    renamed observer to tableObserver

--- automateLinksExtension.js
+++ automateLinksExtension.js
@@ -11,7 +11,7 @@ let checkRows = (records) => {
     }
 }
 
-let observer = new MutationObserver(checkRows)
+let tableObserver = new MutationObserver(checkRows)
 observer.observe(
     table,
     {
