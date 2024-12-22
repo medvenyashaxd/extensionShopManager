37eb652019f49caef55b796825ff669db812e154
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-12-22 22:09:56 +0300

    added ui.js

---
+++ ui.js
@@ -0,0 +1,12 @@
+document.getElementById("autoPlusManualLinks").onchange = function () {
+    console.log(this.checked, "autoPlusManualLinks");
+};
+document.getElementById("manualLinks").onchange = function () {
+    console.log(this.checked, "manualLinks");
+};
+document.getElementById("autoLinks").onchange = function () {
+    console.log(this.checked, "autoLinks");
+};
+document.getElementById("checkJournalLinks").onchange = function () {
+    console.log(this.checked, "checkJournalLinks");
+};
