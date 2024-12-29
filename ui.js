5ff57b24d58ed70579b7309819535f6cc3a5bdb6
Mikhail Holidau <xmedvenyashax@gmail.com>
2024-12-29 23:23:11 +0300

    test enable/disable observer

--- ui.js
+++ ui.js
@@ -1,7 +1,9 @@
 let autoPlusManualLinksStatus = document.getElementById("autoPlusManualLinks");
 let manualLinksStatus = document.getElementById("manualLinks");
 let autoLinksStatus = document.getElementById("autoLinks");
-let checkJournalLinksStatus = document.getElementById("checkJournalLinks")
+let checkJournalLinksStatus = document.getElementById("checkJournalLinks");
+let tableObserver;
+let modalObserver;
 
 function loadCheckboxState() {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
@@ -30,37 +32,13 @@ function saveCheckboxState(scriptName, status) {
     });
 }
 
-function testDisableObs() {
-    tableObserver.disconnect()
-    modalObserver.disconnect()
-}
-
-function testRunObs () {
-    let tableObserver = new MutationObserver(checkRows)
-tableObserver.observe(
-    table,
-    {
-        childList: true,
-        subtree: true
-    }
-)
-
-let modalObserver = new MutationObserver(checkModalResults)
-modalObserver.observe(
-    productModalResults,
-    {
-        childList: true,
-    }
-)
-}
-
 function enableOrDisableScript(scriptName, state, tab) {
     if (scriptName === "autoPlusManualLinks" && state.autoPlusManualLinks) {
         alert("запуск autoPlusManualLinks")
         chrome.scripting.executeScript(
             {
                 target: {tabId: tab, allFrames: false},
-                files: ["manual + auto.js"],
+                func: testEnablObs,
             },
         );
     } else {
@@ -82,3 +60,120 @@ autoPlusManualLinksStatus.onchange = () => {
 manualLinksStatus.onchange = saveCheckboxState;
 autoLinksStatus.onchange = saveCheckboxState;
 checkJournalLinksStatus.onchange = saveCheckboxState;
+
+function testEnablObs() {
+    let productModalResults = document.querySelector(".productModalResults")
+    let table = document.querySelector("tbody")
+
+    function checkModalResults(records) {
+        for (let record of records) {
+            let nodes = record.addedNodes
+            if (nodes.length) {
+                parsingAllTdInfo(nodes[0].querySelectorAll('td.issue-info'))
+            }
+        }
+    }
+
+    let checkRows = (records) => {
+        for (let record of records) {
+            if (record.addedNodes.length) {
+                let element = document.getElementById(record.addedNodes[0].data)
+                if (element) {
+                    parsingRowWords(element)
+                }
+            }
+        }
+    }
+
+    function removeBlackCharacters(str) {
+        return str.replace(/"/g, "").replace(/,/g, "").replace(/;/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/\(/g, "").replace(/\)/g, "").trim()
+    }
+
+    function parsingAllTdInfo(productsListHtml) {
+        let inputProductNameText = removeBlackCharacters(document.querySelector('input.productModalQuery').value)
+        for (let htmlElem of productsListHtml) {
+            let smallElem = htmlElem.querySelector('small')
+            let productNameText = removeBlackCharacters(smallElem.textContent)
+            insertCommonWordsInTdInfo(smallElem, getCommonWords(productNameText.split(" "), inputProductNameText, true))
+        }
+    }
+
+    function getRowText(elem) {
+        let brandText = removeBlackCharacters(elem.querySelector(".brandAndName").innerText)
+        let linkedText = removeBlackCharacters(elem.querySelector(".btn.btn-xs.btn-outline.btn-primary").text)
+        if (brandText.includes("\n")) {
+            brandText = brandText.split("\n")[1]
+        }
+        return {
+            brandText: brandText,
+            linkedText: linkedText,
+        }
+    }
+
+    function parsingRowWords(elem) {
+        let words = getRowText(elem)
+        let brandText = getCommonWords(words.brandText.split(" "), words.linkedText)
+        let linkedText = getCommonWords(words.linkedText.split(" "), words.brandText)
+        insertRowTextInElement(elem, brandText, linkedText)
+    }
+
+    function insertRowTextInElement(elem, brandText, linkedText) {
+        let brandElem = elem.querySelector(".brandAndName")
+        brandElem.querySelector("small") ? brandElem.innerHTML = `<small>${brandElem.querySelector("small").outerText}</small><br>${brandText}` : brandElem.innerHTML = brandText
+
+        let linkedElem = elem.querySelector(".btn.btn-xs.btn-outline.btn-primary")
+        linkedElem.innerHTML = '<i class="fa fa-external-link"></i> ' + linkedText
+    }
+
+    function searchIncludesText(words, word) {
+        words = words.toLowerCase().split(" ")
+        word = word.toLowerCase()
+        return words.includes(word)
+    }
+
+    function insertCommonWordsInTdInfo(htmlElem, commonWords) {
+        htmlElem.getElementsByTagName("a")[0].innerHTML = commonWords
+    }
+
+    function getCommonWords(words1, words2, td) {
+        let commonWords = ""
+        for (let word of words1) {
+            if (searchIncludesText(words2, word)) {
+                commonWords += insertStyle(true, word, td)
+            } else {
+                commonWords += insertStyle(false, word, td)
+            }
+        }
+        return commonWords
+    }
+
+    function insertStyle(common, word, td) {
+        if (common) {
+            return `<span style="background-color: yellow; color: #222;${td ? 'font-weight: bold;' : ''}">${word}</span> `;
+        } else {
+            return `<span>${word}</span> `;
+        }
+    }
+
+    tableObserver = new MutationObserver(checkRows)
+    tableObserver.observe(
+        table,
+        {
+            childList: true,
+            subtree: true
+        }
+    )
+
+    modalObserver = new MutationObserver(checkModalResults)
+    modalObserver.observe(
+        productModalResults,
+        {
+            childList: true,
+        }
+    )
+}
+
+function testDisableObs() {
+    tableObserver.disconnect()
+    modalObserver.disconnect()
+}
