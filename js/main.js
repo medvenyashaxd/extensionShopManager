40df80959a4b3d461e0372d04b2bac2193f3f921
Mikhail Holidau <xmedvenyashax@gmail.com>
2025-01-28 21:36:17 +0300

    optimized manualLinks code for extension

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-const productModalResults = document.querySelector(".productModalResults");
-const table = document.querySelector("tbody");
-
-function searchIncludesText(words, word) {
-    words = words.toLowerCase().split(" ");
-    word = word.toLowerCase();
-    return words.includes(word)
-}
-
 function removeBlackCharacters(str) {
     return str.replace(/"/g, "").replace(/,/g, "").replace(/;/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/\(/g, "").replace(/\)/g, "").trim();
 }
 
-function insertStyle(common, word, td = false) {
+function insertStyle(common, word, td) {
     if (common) {
         return `<span style="background-color: yellow; color: #222;${td ? 'font-weight: bold;' : ''}">${word}</span> `;
     } else {
         return `<span>${word}</span> `;
     }
+}
+
+function searchIncludesText(words, word) {
+    words = words.toLowerCase().split(" ");
+    word = word.toLowerCase();
+    return words.includes(word)
 }
 
 function getCommonWords(words1, words2, td = false) {
--- js/manualLinks.js
+++ js/manualLinks.js
@@ -1,26 +1,30 @@
-function checkModalResults(records) {
-    for (let record of records) {
-        let nodes = record.addedNodes;
-        if (nodes.length) {
-            parsingAllTdInfo(nodes[0].querySelectorAll('td.issue-info'));
+function manualLinks(){
+    function parsingAllTdInfo(productsListHtml) {
+        let inputProductNameText = removeBlackCharacters(document.querySelector('input.productModalQuery').value);
+        for (let htmlElem of productsListHtml) {
+            let smallElem = htmlElem.querySelector('small');
+            let productNameText = removeBlackCharacters(smallElem.textContent);
+            insertCommonWordsInTdInfo(smallElem, getCommonWords(productNameText.split(" "), inputProductNameText, true));
         }
     }
-}
 
-function parsingAllTdInfo(productsListHtml) {
-    let inputProductNameText = removeBlackCharacters(document.querySelector('input.productModalQuery').value);
-    for (let htmlElem of productsListHtml) {
-        let smallElem = htmlElem.querySelector('small');
-        let productNameText = removeBlackCharacters(smallElem.textContent);
-        insertCommonWordsInTdInfo(smallElem, getCommonWords(productNameText.split(" "), inputProductNameText, true));
+    function insertCommonWordsInTdInfo(htmlElem, commonWords) {
+        htmlElem.getElementsByTagName("a")[0].innerHTML = commonWords;
     }
+
+    let modalObserver = new MutationObserver((records) => {
+        for (let record of records) {
+            let nodes = record.addedNodes;
+            if (nodes.length) {
+                parsingAllTdInfo(nodes[0].querySelectorAll('td.issue-info'));
+            }
+        }
+    });
+    modalObserver.observe(
+        document.querySelector(".productModalResults"),
+        {
+            childList: true,
+        });
 }
 
-function insertCommonWordsInTdInfo(htmlElem, commonWords) {
-    htmlElem.getElementsByTagName("a")[0].innerHTML = commonWords;
-}
-
-let modalObserver = new MutationObserver(checkModalResults);
-modalObserver.observe(productModalResults, {
-    childList: true,
-});
+manualLinks();
