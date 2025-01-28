c83f2fc096209ecdb91e01246094fed328025cc9
Mikhail Holidau <xmedvenyashax@gmail.com>
2025-01-28 21:53:08 +0300

    optimized journalLinks code for extension

--- js/journalLinks.js
+++ js/journalLinks.js
@@ -1,27 +1,30 @@
-function checkLinks() {
-    parsingLinks(document.querySelectorAll('td[aria-describedby="links_table_productName"]'), document.querySelectorAll('td[aria-describedby="links_table_linkedProductName"]'));
-    parsingLinks(document.querySelectorAll('td[aria-describedby="links_table_linkedProductName"]'), document.querySelectorAll('td[aria-describedby="links_table_productName"]'),);
-}
-
-function parsingLinks(links1, links2) {
-    for (let i = 0; i < links1.length; i++) {
-        let commonWords = getCommonWords(links1[i].innerText.split(" "), links2[i].innerText);
-        insertCommonWordsInTdInfo(links1[i], commonWords);
+function journalLinks() {
+    function checkLinks() {
+        parsingLinks(document.querySelectorAll('td[aria-describedby="links_table_productName"]'), document.querySelectorAll('td[aria-describedby="links_table_linkedProductName"]'));
+        parsingLinks(document.querySelectorAll('td[aria-describedby="links_table_linkedProductName"]'), document.querySelectorAll('td[aria-describedby="links_table_productName"]'),);
     }
-}
 
-function insertCommonWordsInTdInfo(htmlElem, commonWords) {
-    if (htmlElem.getElementsByTagName("a")[0]) {
-        htmlElem.innerHTML = htmlElem.getElementsByTagName("a")[0].outerHTML + " " + commonWords;
-    } else {
-        htmlElem.innerHTML = commonWords;
+    function parsingLinks(links1, links2) {
+        for (let i = 0; i < links1.length; i++) {
+            let commonWords = getCommonWords(links1[i].innerText.split(" "), links2[i].innerText);
+            insertCommonWordsInTdInfo(links1[i], commonWords);
+        }
     }
+
+    function insertCommonWordsInTdInfo(htmlElem, commonWords) {
+        if (htmlElem.getElementsByTagName("a")[0]) {
+            htmlElem.innerHTML = htmlElem.getElementsByTagName("a")[0].outerHTML + " " + commonWords;
+        } else {
+            htmlElem.innerHTML = commonWords;
+        }
+    }
+
+    checkLinks();
+
+    let tableObserver = new MutationObserver(checkLinks);
+    tableObserver.observe(document.querySelector("tbody"), {
+        childList: true,
+    });
 }
 
-checkLinks();
-
-
-let tableObserver = new MutationObserver(checkLinks);
-tableObserver.observe(table, {
-    childList: true,
-});
+journalLinks();
